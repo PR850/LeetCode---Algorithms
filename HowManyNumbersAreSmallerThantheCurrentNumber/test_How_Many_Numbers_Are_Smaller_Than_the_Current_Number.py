ccaa62cf3903371e2c644fa71d8a1fa4bf884de4
connyneusz <rutek.generaly@gmail.com>
2020-03-08 20:01:06 +0100

    Update test_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py

--- HowManyNumbersAreSmallerThantheCurrentNumber/test_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
+++ HowManyNumbersAreSmallerThantheCurrentNumber/test_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 from How_Many_Numbers_Are_Smaller_Than_the_Current_Number import smallerNumbersThanCurrent
 
 # python -m unittest test_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
@@ -14,22 +15,16 @@ class TestHowManySmaller(unittest.TestCase):
         with self.assertRaises(IndexError):
             smallerNumbersThanCurrent([])
 
-    def test_HowManySmaller(self):
-        result = smallerNumbersThanCurrent([8, 1, 2, 2, 3])
-        self.assertEqual(result, [4, 0, 1, 1, 3])
 
-    def test_HowManySmaller_2(self):
-        result = smallerNumbersThanCurrent([7, 7, 7, 7])
-        self.assertEqual(result, [0, 0, 0, 0])
+test_data = [
+    ([8, 1, 2, 2, 3], [4, 0, 1, 1, 3]),
+    ([7, 7, 7, 7], [0, 0, 0, 0]),
+    ([-1, 1, 4, 5], [0, 1, 2, 3]),
+    ([-1, 5, 4, 2], [0, 3, 2, 1]),
+    ([1], [0])
+]
 
-    def test_HowManySmaller_3(self):
-        result = smallerNumbersThanCurrent([-1, 1, 4, 5])
-        self.assertEqual(result, [0, 1, 2, 3])
 
-    def test_HowManySmaller_4(self):
-        result = smallerNumbersThanCurrent([-1, 5, 4, 2])
-        self.assertEqual(result, [0, 3, 2, 1])
-
-    def test_HowManySmaller_5(self):
-        result = smallerNumbersThanCurrent([1])
-        self.assertEqual(result, [0])
+@pytest.mark.parametrize("test_input, expected_result", test_data)
+def testNumbers(test_input, expected_result):
+    assert smallerNumbersThanCurrent(test_input) == expected_result
